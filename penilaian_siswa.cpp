8c9ee07bcebfb6c9b827f5f98a47216efe2744e1
akmalhudori <60354015+akmalhudori@users.noreply.github.com>
2022-02-15 01:39:23 +0700

    Update penilaian_siswa.cpp

--- penilaian_siswa.cpp
+++ penilaian_siswa.cpp
@@ -33,3 +33,41 @@ int main(){
 		cout<<"nilai akhir     : "<<akhir[i]<<endl;
 		cout<<"============================="<<endl;
 	}
+	
+}#include <iostream>
+using namespace std;
+
+int main(){
+	char nama [100][100];
+	float nilai_ph[100], nilai_pts[100], nilai_pas[100], rata[100], akhir[100], no;
+	
+	cout<<"masukan jumlah siswa : ";
+	cin>>no;
+	
+	for (int i=1; i<=no; i++) {
+		cout<<"masukan nama siswa " <<i<<" : ";
+		cin>>	nama[i];
+		
+		cout<<"masukan nilai PH  : ";
+		cin>>	nilai_ph[i];
+		
+		cout<<"masukan nilai PTS : ";
+		cin>> 	nilai_pts[i];
+		
+		cout<<"masukan nilai PAS : ";
+		cin >>	nilai_pas[i];
+		
+	}
+
+
+	for (int i=1; i<=no; i++){
+		cout<<"============================="<<endl;
+		cout<< "Nama Siswa" <<i<<" : "<<nama[i]<<endl;
+		rata[i]=(nilai_ph[i]+nilai_pts[i]+nilai_pas[i])/3;
+		akhir[i]=(2*rata[i]+nilai_pts[i]+nilai_pas[i])/4;
+		cout<<"nilai rata-rata : "<<rata[i]<<endl;
+		cout<<"nilai akhir     : "<<akhir[i]<<endl;
+		cout<<"============================="<<endl;
+	}
+	
+}
